eae5cf60335a91a19a72af2e5cf47f218e2301de
Omar <ogspain@gmail.com>
2019-10-10 20:32:00 -0400

    Initial commit on Vue Django integration package

---
+++ __init__.py
@@ -0,0 +1,7 @@
+r"""
+"""
+
+__title__ = 'Vue Django'
+__version__ = '1.0'
+__author__ = 'Omar España'
+__license__ = 'MIT'
---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VueDjanfoConfig(AppConfig):
+    name = 'vue_django'
+    verbose_name = "Vue Django"
+
