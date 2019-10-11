7a03a7dfbd7f43205fd01f5fab3c778de647381a
Omar <ogspain@gmail.com>
2019-10-11 01:37:00 -0400

    Addign global authentication variables to the context

--- __init__.py
+++ __init__.py
@@ -5,3 +5,6 @@ __title__ = 'Vue Django'
 __version__ = '1.0'
 __author__ = 'Omar España'
 __license__ = 'MIT'
+
+
+default_app_config = 'vue_django.RestFrameworkConfig'
--- utils.py
+++ utils.py
@@ -2,6 +2,20 @@ from django.conf import settings
 
 from django.middleware.csrf import get_token
 
+
+def get_authentication():
+    on_login= getattr(settings, "LOGIN_URL", None )
+    on_logout= getattr(settings, "LOGOUT_REDIRECT_URL", None )
+    redir= getattr(settings, "LOGIN_REDIRECT_URL", None)
+    auth_path= getattr(settings, "DLV_AUTH_PATH", None)
+    #
+    return {
+        'on_logout':on_logout,
+        'on_login':redir,
+        'auth_path':auth_path
+    }
+
+
 def get_context_object(request):
     
     if request.user.is_authenticated:
@@ -17,6 +31,10 @@ def get_context_object(request):
     else
         user = request.user.is_authenticated
 
-    context =  {'user':user, 'token':get_token(request) }
+
+    context =  {
+        'auth':get_authentication,
+        'user':user, 
+        'token':get_token(request) }
 
     return {'vue_django_vars':mark_safe(json.dumps(context))}
