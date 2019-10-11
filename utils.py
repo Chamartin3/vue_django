37f1b3f2450f4b1ac7a79c99bb5d2242cf7fdd7a
Omar <ogspain@gmail.com>
2019-10-10 23:04:00 -0400

    creates the context dictionary and the integrates the autenticated user and the CSRF Token

--- utils.py
+++ utils.py
@@ -5,9 +5,16 @@ from django.middleware.csrf import get_token
 def get_context_object(request):
     
     if request.user.is_authenticated:
-        user = request.user
+        user= {
+        'id':request.user.pk,
+        'username':request.user.username,
+        'first_name':request.user.first_name,
+        'last_name':request.user.last_name,
+        'email':request.user.email
+        }
     else
         user = request.user.is_authenticated
 
-    return {
-        'user':user, 'token':get_token(request)
+    context =  {'user':user, 'token':get_token(request) }
+
+    return {'vue_django_vars':mark_safe(json.dumps(context))}
