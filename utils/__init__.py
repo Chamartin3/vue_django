0f586b2f1342ef6ead32947df961b8c92f24efd1
Omar <ogspain@gmail.com>
2019-10-25 10:48:00 -0400

    refactoring the Utils global variables

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,25 @@
 from django.conf import settings
-
+from django.utils.encoding import smart_str
 from .serializers import GroupsSerializer, PermissionSerializer
-
 from .api_map import ApiMap
 
-from django.utils.encoding import smart_str
+
+# Autentication GlobalVariables
+LOGIN_URL = getattr(settings, "LOGIN_URL", None )
+LOGOUT_REDIRECT_URL = getattr(settings, "LOGOUT_REDIRECT_URL", None )
+LOGIN_REDIRECT_URL = getattr(settings, "LOGIN_REDIRECT_URL", None)
+
+# Vue Django GlobalVariables
+DLV_AUTH_PATH = getattr(settings, "DLV_AUTH_PATH", None)
+DJV_NAVIGATION = getattr(settings, "DJV_NAVIGATION", [''])
+DJV_EXCLUDED_ROUTES = getattr(settings, "DJV_EXCLUDED_ROUTES", [''])
+DJV_API_ROUTE = getattr(settings, "DJV_API_ROUTE", 'api/')
 
 def get_authentication():
-    on_login= getattr(settings, "LOGIN_URL", None )
-    on_logout= getattr(settings, "LOGOUT_REDIRECT_URL", None )
-    redir= getattr(settings, "LOGIN_REDIRECT_URL", None)
-    auth_path= getattr(settings, "DLV_AUTH_PATH", None)
+    on_login = LOGIN_URL
+    on_logout = LOGOUT_REDIRECT_URL
+    redir = LOGIN_REDIRECT_URL
+    auth_path = DLV_AUTH_PATH
     #
     return {
         'on_logout':on_logout,
@@ -28,27 +37,26 @@ def get_auth_user(request):
         'last_name':request.user.last_name,
         'email':request.user.email,
         'permissions':PermissionSerializer(request.user.user_permissions.all(), many=True).data,
-        'groups':GroupsSerializer(request.user.groups.all(), many=True).data,
-        # 'picture':request.user.profile.pic_url
+        'groups':GroupsSerializer(request.user.groups.all(), many=True).data
         }
     else:
         user=request.user.is_authenticated
-    # returns all the Autentication information
     return user
 
 
 def get_routes(api):
     ''' gets all the paths that interacts with the aplication '''
 
-    # Paths that have an asociated route (this are also the paths where a aplication is redirectes if the vue routed does not match a page )
+    # Paths that have an asociated route (this are also the 
+    # paths where a aplication is redirectes if the vue routed does not match a page )
     # TODO: filter these by autehcticationm
-    navigation_paths= getattr(settings, "DJV_NAVIGATION", [''])
+    navigation_paths= DJV_NAVIGATION
 
     # Paths that are excluded from the api
-    excluded_routes= getattr(settings, "DJV_EXCLUDED_ROUTES", [''])
+    excluded_routes= DJV_EXCLUDED_ROUTES
 
     # base route API (is the url where the api es located)I
-    api_baseroute= getattr(settings, "DJV_API_ROUTE", 'api/')
+    api_baseroute= DJV_API_ROUTE
 
     redir_paths=[{ 'redirect':f'/{p}', 'path': f'/{p}*'} for p in navigation_paths]
 
