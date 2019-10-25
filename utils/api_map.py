9d27007816d14709da3b57fac2d4acd8c76252ed
Omar <ogspain@gmail.com>
2019-10-25 10:12:00 -0400

    Simplifying the procesong and generation of the map

--- utils/api_map.py
+++ utils/api_map.py
@@ -1,97 +1,85 @@
-class APIMap(object):
-    """APIMap  utility class that process ana endpoint and 
-    returns a list with all the actions methods an lists contian each one """
-
-    def __init__(self, api):
-        self.api = api
-        self.modules = self.processAPIPaths(api)
-        self.modelMap = self.generateModels()
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 
-
-    def get_model_fields(self, serializer_class):
-        '''
-        TODO: read the fields and generate a list of validators that can be replicated 
-        in the vue aplication
-        '''
-        instance = serializer_class()
-        fields = {}
-        relation_classes = [
-            HyperlinkedIdentityField,
-            HyperlinkedRelatedField,
-            ManyRelatedField,
-            PrimaryKeyRelatedField,
-            RelatedField,
-            SlugRelatedField,
-            StringRelatedField,
-        ]
-
-        try:
-            for f in instance._writable_fields:
-                field={}
-                field['related'] = False
-                field['_kwargs'] = [ str(x) for x in getattr( f, '_kwargs', []) ]
-                field['label'] = str(getattr( f, 'label', None))
-                field['help_text'] = str(getattr( f, 'help_text', None))
-                field['allow_null']= getattr( f, 'allow_null', False)
-                field['allow_blank']= getattr( f, 'allow_blank', False)
-                field['read_only']= getattr( f, 'read_only', False)
-                validators=[v.__class__.__name__ for v in getattr(f, '_validators', [])]
-                field['_validators'] = validators
-                fields[f.field_name] = field
-
-            for rf in instance._readable_fields:
-                if rf.__class__ in relation_classes:
-                    # import pdb; pdb.set_trace()
-                    field['related']=True
-
-                    child_relation=getattr(rf, 'child_relation', None)
-                    if child_relation is not None:
-                        qs=getattr(child_relation, 'queryset', None)
-                    else:
-                        qs=None
-
-                    if qs is not None:
-                        field['related_model']=qs.model.__name__
-                    else:
-                        field['related_model']=rf.field_name
-
-                    field['_kwargs']=[str(x) for x in getattr(rf, '_kwargs', [])]
-                    field['label']=str(getattr(rf, 'label', None))
-                    field['help_text']=str(getattr(rf, 'help_text', None))
-                    field['required']=getattr(rf, 'required', False)
-                    validators=[v.__class__.__name__ for v in getattr(rf, '_validators', [])]
-                    field['_validators']=validators
-                    fields[rf.field_name]=field
-
-                elif issubclass(rf.__class__, ModelSerializer):
-                    field['related']=True
-                    field['related_model']=rf.Meta.model.__name__
-                    field['_kwargs']=[str(x) for x in getattr(f, '_kwargs', [])]
-                    field['label']=str(getattr(f, 'label', None))
-                    field['help_text']=str(getattr(f, 'help_text', None))
-                    field['allow_null']=getattr(f, 'allow_null', False)
-                    field['allow_blank']=getattr(f, 'allow_blank', False)
-                    field['read_only']=getattr(f, 'read_only', False)
-                    validators=[v.__class__.__name__ for v in getattr(f, '_validators', [])]
-                    field['_validators']=validators
-                    fields[f.field_name]=field
-
+def get_model_fields(serializer_class):
+    '''
+    TODO: read the fields and generate a list of validators that can be replicated 
+    in the vue aplication
+    '''
+    instance = serializer_class()
+    fields = {}
+    relation_classes = [
+        HyperlinkedIdentityField,
+        HyperlinkedRelatedField,
+        ManyRelatedField,
+        PrimaryKeyRelatedField,
+        RelatedField,
+        SlugRelatedField,
+        StringRelatedField,
+    ]
+    try:
+        for f in instance._writable_fields:
+            field={}
+            field['related'] = False
+            field['_kwargs'] = [ str(x) for x in getattr( f, '_kwargs', []) ]
+            field['label'] = str(getattr( f, 'label', None))
+            field['help_text'] = str(getattr( f, 'help_text', None))
+            field['allow_null']= getattr( f, 'allow_null', False)
+            field['allow_blank']= getattr( f, 'allow_blank', False)
+            field['read_only']= getattr( f, 'read_only', False)
+            validators=[v.__class__.__name__ for v in getattr(f, '_validators', [])]
+            field['_validators'] = validators
+            fields[f.field_name] = field
+        for rf in instance._readable_fields:
+            if rf.__class__ in relation_classes:
+                # import pdb; pdb.set_trace()
+                field['related']=True
+                child_relation=getattr(rf, 'child_relation', None)
+                if child_relation is not None:
+                    qs=getattr(child_relation, 'queryset', None)
                 else:
-                    if rf not in instance._writable_fields and rf.field_name is not 'id':
-                        # print(rf.field_name)
-                        pass
-        except Exception as e:
-            # raise e
-            # print(e)
-            # print(serializer_class)
-            pass
-        return fields
+                    qs=None
+                if qs is not None:
+                    field['related_model']=qs.model.__name__
+                else:
+                    field['related_model']=rf.field_name
+                field['_kwargs']=[str(x) for x in getattr(rf, '_kwargs', [])]
+                field['label']=str(getattr(rf, 'label', None))
+                field['help_text']=str(getattr(rf, 'help_text', None))
+                field['required']=getattr(rf, 'required', False)
+                validators=[v.__class__.__name__ for v in getattr(rf, '_validators', [])]
+                field['_validators']=validators
+                fields[rf.field_name]=field
+            elif issubclass(rf.__class__, ModelSerializer):
+                field['related']=True
+                field['related_model']=rf.Meta.model.__name__
+                field['_kwargs']=[str(x) for x in getattr(f, '_kwargs', [])]
+                field['label']=str(getattr(f, 'label', None))
+                field['help_text']=str(getattr(f, 'help_text', None))
+                field['allow_null']=getattr(f, 'allow_null', False)
+                field['allow_blank']=getattr(f, 'allow_blank', False)
+                field['read_only']=getattr(f, 'read_only', False)
+                validators=[v.__class__.__name__ for v in getattr(f, '_validators', [])]
+                field['_validators']=validators
+                fields[f.field_name]=field
+            else:
+                if rf not in instance._writable_fields and rf.field_name is not 'id':
+                    # print(rf.field_name)
+                    pass
+    except Exception as e:
+        # raise e
+        # print(e)
+        # print(serializer_class)
+        pass
+    return fields
 
-
-    # -----------------------------------------------------------
-    #   
-    # -----------------------------------------------------------
+class APIReader(object):
+    """
+        API Reader Reads the routes and gerates a model
+    """
+    def __init__(self, api):
+        self.modules = self.processAPIPaths(api)
 
     def simplifiedEndpoint(self, pattern):
         '''
@@ -115,7 +103,7 @@ class APIMap(object):
             endpoint['endpoint'] = self.simplifiedEndpoint(path.pattern._regex)
             methods = [ k.upper() for k in path.callback.actions.keys() ]
             endpoint['methods'] = methods
-            endpoint['fields'] = self.get_model_fields(view.serializer_class)
+            endpoint['fields'] = get_model_fields(view.serializer_class)
 
 
         elif issubclass(path.callback.cls, APIView):
@@ -124,11 +112,10 @@ class APIMap(object):
             methods=[m.upper() for m in view.http_method_names if hasattr(view, m) and m.upper() != 'OPTIONS']
             endpoint['methods'] = methods
             if hasattr(view, 'serializer_class'):
-                endpoint['fields'] = self.get_model_fields(view.serializer_class)
+                endpoint['fields'] = get_model_fields(view.serializer_class)
             else:
                 endpoint['fields'] = None
         return endpoint
-
 
     def processSubRoutes(self, sub_route):
         '''
@@ -157,7 +144,6 @@ class APIMap(object):
         module['module_path'] = path.pattern._regex
         module['actions'] = actions
         return module
-
     
     def getName(self, path):
         '''
@@ -181,6 +167,18 @@ class APIMap(object):
 
         # Route Reader
 
+
+
+class APIMap(object):
+    """APIMap  utility class that process ana endpoint and 
+    returns a list with all the actions methods an lists contian each one """
+
+    def __init__(self, api):
+        reader = APIReader(api)
+        self.modules = reader.modules
+        self.api = api
+        self.modelMap =  self.generateModels()
+    
 
     def processFullpath(self, fullpath):
         '''
@@ -225,13 +223,12 @@ class APIMap(object):
                     act['unique'] = methods_per_action < 2
                     act['type'] = m
                     act['basepath'] = module['module_path']
-                    act['route'] = self.processFullpath(module['module_path']+action['route'])
+                    act['route'] = self.processFullpath(module['module_path'] + action['route'])
                     act['fields'] = action['fields']
                     act['endpoint_name'] = a_name
                     act['module_name'] = m_name
                     actions.append(act)
         return actions
-
 
     def getNamespace(self, a):
         '''
@@ -241,8 +238,6 @@ class APIMap(object):
         if len(ep)>1:
             return True, ep[0]
         return  False ,a['module_name']
-
-
 
     def generateModels(self):
         ''' Generates the final iteration of the models
