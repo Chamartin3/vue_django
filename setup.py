db0e7471b3193c7e4e49d547245a38908675637d
Omar <ogspain@gmail.com>
2019-11-01 15:00:44 -0400

    Reestructuring the proyect aplying Setup tools and preparing for distributionç

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='vue_django',
+      version='0.1',
+      description='Django aplication mapper to use with a Vue front end aplication and Django Rest Framework',
+      url='https://github.com/Chamartin3/vue_django.git',
+      author='Omar España,
+      author_email='omidev22@gmail.com',
+      license='MIT',
+      packages=['vue_django'])
